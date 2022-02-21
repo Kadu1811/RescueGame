99f4be79aaddcf2af1163dff73f9a16d090bce24
Kadu1811 <kadu.dear60@gmail.com>
2022-02-21 18:19:42 -0300

    adicionando movimento para 'amigo' e caminhao inimigo

--- js/js.js
+++ js/js.js
@@ -34,7 +34,8 @@ function start() {
 	movefundo();
     movejogador();
     moveinimigo1();
-    
+    moveinimigo2();
+    moveamigo();
 	
 	}
 
@@ -86,6 +87,27 @@ function start() {
             $("#inimigo1").css("top",posicaoY);
                 
             }
+    }
+
+    function moveinimigo2() {
+
+        posicaoX = parseInt($("#inimigo2").css("left"));
+	    $("#inimigo2").css("left",posicaoX-3);
+				
+		if (posicaoX<=0) {
+		    $("#inimigo2").css("left",775);		
+		}
+    }
+
+    function moveamigo() {
+	
+        posicaoX = parseInt($("#amigo").css("left"));
+        $("#amigo").css("left",posicaoX+1);
+                    
+        if (posicaoX>906) { 
+            $("#amigo").css("left",0);       
+        }
+    
     }
 
 }
