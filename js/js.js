f5ee6d541b38ebf0f2a4f3cd7b4cb86873849136
Kadu1811 <kadu.dear60@gmail.com>
2022-02-21 18:17:14 -0300

    criando movimentação do helicoptero inimigo

--- js/js.js
+++ js/js.js
@@ -8,6 +8,8 @@ function start() {
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
     var jogo = {};
+    var velocidade=5;
+    var posicaoY = parseInt(Math.random() * 334);
     var TECLA = {
         W: 87,
         S: 83,
@@ -31,6 +33,8 @@ function start() {
 	
 	movefundo();
     movejogador();
+    moveinimigo1();
+    
 	
 	}
 
@@ -68,6 +72,20 @@ function start() {
             //Chama função Disparo	
         }
     
+    }
+
+    function moveinimigo1() {
+
+        posicaoX = parseInt($("#inimigo1").css("left"));
+        $("#inimigo1").css("left",posicaoX-velocidade);
+        $("#inimigo1").css("top",posicaoY);
+            
+            if (posicaoX<=0) {
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left",694);
+            $("#inimigo1").css("top",posicaoY);
+                
+            }
     }
 
 }
